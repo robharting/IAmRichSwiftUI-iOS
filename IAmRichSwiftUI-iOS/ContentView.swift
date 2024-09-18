5f982247e64e0aec6e14d923a27ebddcf8d210f8
Rob Harting <robharting@hotmail.com>
2024-09-18 08:54:11 +0200

    via swiftUI views and modifiers set image and text properties

--- IAmRichSwiftUI-iOS/ContentView.swift
+++ IAmRichSwiftUI-iOS/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+                
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
