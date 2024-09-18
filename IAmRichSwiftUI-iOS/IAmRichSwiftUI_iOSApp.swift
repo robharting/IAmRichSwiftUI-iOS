abbde2b0e45c7064c44b1b4dec0ce91423300c27
Rob Harting <robharting@hotmail.com>
2024-09-18 08:06:30 +0200

    Initial Commit

---
+++ IAmRichSwiftUI-iOS/IAmRichSwiftUI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUI_iOSApp.swift
+//  IAmRichSwiftUI-iOS
+//
+//  Created by Harting, R.P.G. (Rob) on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
